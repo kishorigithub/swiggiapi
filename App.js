4647daeb2da3f8187a7c831883ed80183280fb49
Kishori <salvikishori96@gmail.com>
2024-08-07 19:04:48 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+// const heading= React.createElement("h1",{},"Hello world from react");
+
+// const root= ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading)
+
+const createnew = React.createElement("div",{},
+    [React.createElement("div",{},[React.createElement("h1",{},"I am nested h1 tag"),
+        React.createElement("h2",{},"I am nested h2 tag")]),React.createElement("div",{},[React.createElement("h1",{},"I am nested h1 tag"),
+            React.createElement("h2",{},"I am nested h2 tag")])])
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(createnew)
